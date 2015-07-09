6ffe38f2744eb1bb882bd7ab8358c3f02caf405b
qwb02030 <qwb02030@gmail.com>
2015-07-09 22:34:47 +0900

    Add Contact Static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -53,4 +53,19 @@ require 'spec_helper'
 
     end
 
+    describe "Contact" do
+
+      it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_content('Contact')
+      end
+
+      it "should have the title 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      end
+
+    end
+
+
   end
